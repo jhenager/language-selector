78cac1ae47825e3358293c551bf2a5bbf3589fff
James Henager <jameschenager@gmail.com>
2020-06-05 15:41:37 -0700

    add image for ruby and python

--- js/scripts.js
+++ js/scripts.js
@@ -43,7 +43,7 @@ $(document).ready(function(){
         $("div#python").show();
       } else if (animal === "dog") {
         $("form#language").hide();
-        $("div#javscript").show();
+        $("div#javascript").show();
       }
     }
     
