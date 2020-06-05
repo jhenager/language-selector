6ec7e592d910206e15b3d9a4a621fc9f3fd49584
James Henager <jameschenager@gmail.com>
2020-06-05 10:23:19 -0700

    add first successful if/else logic for beanie check

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,12 @@
 $(document).ready(function(){
   $("form#language").submit(function(event) {
     const age = parseInt($("input#age").val());
-    alert(age);
-
+    if (age>18 && "input[type=radio][value=beanie]:checked"){
+      alert("Beanie is checked");
+    } else{
+      alert("It didn't work")
+    }
+   
     event.preventDefault();
-  });
+  })
 });
