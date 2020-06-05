052c0d1757c8ecf9e4f58e582393a1232257dcfb
James Henager <jameschenager@gmail.com>
2020-06-05 16:50:10 -0700

    add small changes to clean up look of code and organize

--- js/scripts.js
+++ js/scripts.js
@@ -44,7 +44,7 @@ $(document).ready(function(){
     }
     
     if (name === "") {
-      alert("Please enter your name")
+      alert("Please enter your name");
     }
     event.preventDefault();
   })
