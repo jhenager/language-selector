ca365aa3eb528b89199f40ef4ddbbb2126cc5a3d
James Henager <jameschenager@gmail.com>
2020-06-05 10:54:24 -0700

    add animal variable for jquery check

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,13 @@ $(document).ready(function(){
   $("form#language").submit(function(event) {
     const age = parseInt($("input#age").val());
     const hat = $("input:radio[name=hat]:checked").val();
+    const animal = $("input:radio[name=animal]:checked").val();
 
-    if ((age>18) && (hat === "beanie")) {
+    if ((age>18) && (hat === "beanie") && (animal === "hedgehog")) {
       alert("Beanie is checked");
-    } 
+    } else {
+      alert("it didn't work")
+    }
       
     event.preventDefault();
   })
