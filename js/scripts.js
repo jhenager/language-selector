fdc5e254617982a79cfe379035ec5c55c59b3d93
James Henager <jameschenager@gmail.com>
2020-06-05 11:22:50 -0700

    add ability to hide the form and show a result

--- js/scripts.js
+++ js/scripts.js
@@ -10,9 +10,12 @@ $(document).ready(function(){
     const age = parseInt($("input#age").val());
     const hat = $("input:radio[name=hat]:checked").val();
     const animal = $("input:radio[name=animal]:checked").val();
+    const coffee = $("#coffee").val();
+    const fruit = $("#fruit").val();
 
-    if ((age>18) && (hat === "beanie") && (animal === "hedgehog")) {
-      alert("Beanie is checked");
+    if ((age>=18) && (hat === "beanie") && (fruit === "apple")) {
+      $("form#language").hide();
+      $("div#ruby").show();
     } else {
       alert("it didn't work")
     }
