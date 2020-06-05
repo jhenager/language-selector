9678a4a33004194c93a68594fd76df3565b711ed
James Henager <jameschenager@gmail.com>
2020-06-05 14:00:57 -0700

    add feature to prepend input from name form to result

--- js/scripts.js
+++ js/scripts.js
@@ -7,26 +7,34 @@
 
 $(document).ready(function(){
   $("form#language").submit(function(event) {
-    const age = parseInt($("input#age").val());
+    const name = $("input#name").val();
     const hat = $("input:radio[name=hat]:checked").val();
     const animal = $("input:radio[name=animal]:checked").val();
     const coffee = $("#coffee").val();
     const fruit = $("#fruit").val();
 
+    $("#ruby-name").prepend(name + ", ");
+    $("#python-name").prepend(name + ", ");
+    $("#java-name").prepend(name + ", ");
+
     if (hat === "beanie") {
-      if (animal === "hedgehog") {
+      if ((animal === "hedgehog" || animal === "boa")) {
       $("form#language").hide();
       $("div#ruby").show();
       } else if (coffee === "sweet") {
         $("form#language").hide();
         $("div#ruby").show();
-      } else if ()
+      } 
     }
 
     else {
       alert("it didn't work")
     }
-      
+    
+    
+    
+
+  
     event.preventDefault();
   })
 });
